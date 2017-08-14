81d5b6ef5e95c24d98e89529402bf782023ab76c
Nick Cox <nicholas.cox@t-mark.com>
2017-08-14 17:36:30 -0400

    Bio modal

--- js/app.js
+++ js/app.js
@@ -18,5 +18,35 @@ $(function(){
         $mechanical.removeClass('is-active');
       });
 
+  var $learnMore = $('.learn-more'),
+      $learnContent = $('.learn-content'),
+      $modal = $('.modal'),
+      $modalClose = $('.modal-close');
+
+  $learnMore.on('click', function() {
+    var $this = $(this);
+
+    var name = $this.data('name');
+
+    $learnContent.each(function(){
+      var $that = $(this);
+      var contentName = $that.data('name');
+        if (name === contentName) {
+          $that.addClass('active');
+          $modal.addClass('is-active');
+        }
+    });
+
+    $modalClose.on('click', function(){
+      $learnContent.removeClass('active');
+      $modal.removeClass('is-active');
+    });
+
+
+        
+  });
+
+      
+
 
 });
