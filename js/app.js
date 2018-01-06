2747e37320d2e8bea48cbd1bcd87039cacdeb96d
Nick Cox <nick.cox@twisted-rope.com>
2018-01-06 11:04:27 -0500

    Single Page

--- js/app.js
+++ js/app.js
@@ -1,23 +1,5 @@
 $(function(){
 
-  var $mechanical = $('#mechanical'),
-      $sheet      = $('#sheetmetal'),
-      $mechanicalContent = $('.mechanical-content'),
-      $sheetContent = $('.sheetmetal-content');
-
-      $mechanical.on('click', function(){
-        $mechanicalContent.addClass('active');
-        $sheetContent.removeClass('active');
-        $mechanical.addClass('is-active');
-        $sheet.removeClass('is-active');
-      });
-      $sheet.on('click', function(){
-        $sheetContent.addClass('active');
-        $mechanicalContent.removeClass('active');
-        $sheet.addClass('is-active');
-        $mechanical.removeClass('is-active');
-      });
-
   var $learnMore = $('.learn-more'),
       $learnContent = $('.learn-content'),
       $modal = $('.modal'),
